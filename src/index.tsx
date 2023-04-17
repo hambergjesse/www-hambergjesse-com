a5101399ec9f564f8adebf5bdc64666ad7d3903e
hambergjesse <hambergjesse@gmail.com>
2023-04-17 12:53:10 +0300

    css boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createRoot } from "react-dom/client";
+import "./scss/index.scss";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
